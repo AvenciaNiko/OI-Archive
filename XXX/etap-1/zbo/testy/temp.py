94e871d2355a0bfc4914c2175aae4f55ce0e7936
AvenciaNiko <avencianiko@protonmail.com>
2025-12-24 21:32:50 +0100

    Delete XXX/etap-1/zbo/testy directory

--- XXX/etap-1/zbo/testy/temp.py
+++
@@ -1,9 +0,0 @@
-import random
-
-# n = random.randint(10001, 100000)
-# k = random.randint(2, n)
-
-n = 10
-
-for i in range(n - 1):
-    print(f"{random.randint(1, n)} {random.randint(1, n)} {random.randint(1, 10)}")
